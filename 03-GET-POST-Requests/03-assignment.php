03619d062e960164a97831baeb57ad7a5e413e6a
Multimedia Intro <mmintro@nvc.ad.root>
2018-02-07 19:37:21 -0600

    Initial commit

--- 03-GET-POST-Requests/03-assignment.php
+++ 03-GET-POST-Requests/03-assignment.php
@@ -1,36 +1,60 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Test</title>
+<style>
+  body {
+    font-family:sans-serif;
+  }
+  form {
+    display:block;
+    margin: 10em auto;
+    max-width:300px;
+  }
+</style>
+</head>
+<body>
+ 
 
+
+<!--
 /*****************************************
 *************  Instructions  *************
 ******************************************
          Fill in your responses
         to this assignment below.
 *****************************************/
+-->
 
 
 
-// 01. Create a form with at least 2 text inputs and a submit button
+<!--// 01. Create a form with at least 2 text inputs and a submit button-->
 
-
-
-
-
-// 02. This criterion is linked to a Learning Outcome Display URL parameter value for "first_name" text input
-
-
-
-
-
-// 03. This criterion is linked to a Learning Outcome Display URL parameter value for "email" text input
+<form action="" method="get">
+  <p>Name: <input type="text" name="name"></p>
+  <p>E-mail: <input type="text" name="email"></p>
+<input type="submit">
+</form>
 
 
 
 
+<!--// 02. This criterion is linked to a Learning Outcome Display URL parameter value for "first_name" text input-->
 
-// 04. This criterion is linked to a Learning Outcome Display POST data once form has been submitted
-
-
-
-
-
+<?php
+  
 ?>
+
+<!--// 03. This criterion is linked to a Learning Outcome Display URL parameter value for "email" text input-->
+
+
+
+
+
+<!--// 04. This criterion is linked to a Learning Outcome Display POST data once form has been submitted-->
+<?php
+  
+?>
+</body>
+</html>
