1076a852a30c8c8e5732c6cd3cbb2ef940a51c07
lgallegos39 <xmaster234@gmail.com>
2018-03-05 22:44:50 -0600

    Created session, variables, and echo

--- 04-Sessions/04-assignment.php
+++ 04-Sessions/04-assignment.php
@@ -11,7 +11,23 @@
 
 // 01. Initiate/start a PHP session by using the script
 
+<?php
+// Start the session
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
 
+<?php
+// Set session variables
+$_SESSION["favcolor"] = "green";
+$_SESSION["favanimal"] = "cat";
+echo "Session variables are set.";
+?>
+
+</body>
+</html>
 
 
 
