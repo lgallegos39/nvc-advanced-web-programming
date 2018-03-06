dd0a679ea035afc4bda0e5e615834dace4eb1e0b
lgallegos39 <xmaster234@gmail.com>
2018-03-06 12:22:57 -0600

    Fixed session

--- 04-Sessions/04-assignment.php
+++ 04-Sessions/04-assignment.php
@@ -1,46 +1,38 @@
-<?php
-
+<!--
 /*****************************************
 *************  Instructions  *************
 ******************************************
          Fill in your responses
         to this assignment below.
 *****************************************/
+-->
 
-
-
-// 01. Initiate/start a PHP session by using the script
-
+<!-- 01. Initiate/start a PHP session by using the script-->
 <?php
-// Start the session
 session_start();
 ?>
 <!DOCTYPE html>
 <html>
+<head>
+    <style>
+        
+    </style>
+</head>
 <body>
 
+<!-- 02. Set a value to a session-->
 <?php
-// Set session variables
-$_SESSION["favcolor"] = "green";
-$_SESSION["favanimal"] = "cat";
-echo "Session variables are set.";
+$_SESSION["age"] = "22";
+$_SESSION["name"] = "Luis";
+$_SESSION["favFood"] = "spaghetti";
+?>
+
+<!--// 03. This criterion is linked to a Learning Outcome Echo the session value to display on the page-->
+<?php
+// Echo session variables that were set on previous page
+echo "<h2> Hello " . $_SESSION["name"] . "!</h2>";
+echo "<h3> You are " . $_SESSION["age"] . " years old and your favorite food is " . $_SESSION["favFood"] . ".</h3>";
 ?>
 
 </body>
 </html>
-
-
-
-// 02. Set a value to a session
-
-
-
-
-
-// 03. This criterion is linked to a Learning Outcome Echo the session value to display on the page
-
-
-
-
-
-?>
