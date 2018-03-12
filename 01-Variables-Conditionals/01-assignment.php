0f9fd82255bbd3b999a28bb0f33a173f5eaa5058
lgallegos39 <xmaster234@gmail.com>
2018-03-12 14:16:53 -0500

    Fixed assignment

--- 01-Variables-Conditionals/01-assignment.php
+++ 01-Variables-Conditionals/01-assignment.php
@@ -1,49 +1,37 @@
-<?php
-
-/*****************************************
+<!DOCTYPE html>
+<html lang="en">
+<!--/*****************************************
 *************  Instructions  *************
 ******************************************
          Fill in your responses
         to this assignment below.
-*****************************************/
-
-
-
+*****************************************/-->
+<head>
+   
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
 // 01. Create and use a string variable
-
-
-
-
-
-
+$string = "This is a string</br>";
 // 02. Create and use an integer variable
-
-
-
-
-
+$integer = 10;
 // 03. Create and use a boolean variable
-
-
-
-
-
+$boolean = true;
 // 04. Create and call one or more function(s)
-
-
-
-
-
+function hello() {
+    echo "Hello world!</br>";
+}
+hello();
 // 05. Basic conditional statement
+// 06. Echo to page
+if ($string > $integer) {
+    echo hello();
+} else {
+    echo $string;
+}
 
-
-
-
-
-// 01. Echo to page
-
-
-
-
-
-?>
+?>    
+</body>
+</html>
